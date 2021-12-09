48cba908f6ee416b4725ca64243951319638fbfa
IsakGranlund <Isak.Granlund.0592@student.uu.se>
2021-12-09 11:52:12 +0100

    svarsalternativ

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -58,9 +58,6 @@
           <h1 class="city_name_charachter_spec">Malmö</h1>
         </figure>
       </div>
-
-
-
     </div>
   
   </section>
@@ -95,6 +92,7 @@
       {{ uiLabels.question }}:
       <input type="text" v-model="question">
       <div class="question-multiple">
+
         <img src="">
         {{ uiLabels.answers}}:
         <input v-for="(_, i) in answers"
@@ -102,12 +100,15 @@
                v-bind:key="'answer'+i"
                class="textbox"
                 >
+
         <div>
         <input type="checkbox" v-for="(_, i) in checkBox"
                v-model="checkBox[i]"
                v-bind:key="'checkBox'+i">
         {{this.answers.length}}
         </div>
+
+
         <div>
         <button v-on:click="addAnswer">
           {{ uiLabels.addAnswer}}:
@@ -120,7 +121,38 @@
       <button v-on:click="addQuestion">
         Add question
       </button>
+
+
+      <div class="answer-alternative-size-wrapper" >
+
+        <div id="Answer-Box-symbol-prop" >
+
+        </div>
+
+        <div class="Answer-Box-textarea"  >
+
+          <input
+                  class="Answer-Box-textarea-prop"
+                 placeholder="Answer 1"
+                 >
+
+
+        </div>
+
+      <div class="Answer-Box-checkbox"  >
+
+        <input type="checkbox"
+                class="Answer-Box-checkbox-prop
+                animation_rubberband">
+      </div>
+
+      </div>
+
+
     </div>
+
+
+
     <button>  <router-link class="routerLink" v-bind:to="'/result/'+pollId">Check result</router-link></button>
   </div>
   <div class=" create alternative-right-side">
@@ -257,7 +289,8 @@ export default {
       }
 
     }
-  }
+  },
+
 }
 </script>
 <style>
@@ -287,12 +320,77 @@ export default {
   padding: 0;
 }
 
+
+
+.Answer-Box-textarea-prop{
+
+  width: 100%;
+  height: 97%;
+  margin-left: 2%;
+  float: left;
+  font-size: 18px;
+  font-family: sans-serif;
+  font-weight: 600;
+  outline: none;
+  border: none;
+
+
+
+
+
+
+}
+
+.Answer-Box-checkbox-prop{
+  margin-right: 15%;
+  height: 100%;
+   -webkit-transform: scale(2);
+  transform: scale(2);
+border-radius: 5px;
+}
+
+
 .alternative-right-side {
   display: inline-block;
   float:right;
   right: 200px;
   width: 19%;
   border: 3px solid green;
+
+}
+.animation_rubberband{
+
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+
+  animation: rubberBand  2s infinite;
+}
+.animation_rubberband:hover{
+  animation: 0 ;
+}
+
+
+@keyframes rubberBand {
+  0%{
+    transform: scale3d(2,2,2);
+  }
+  15%{
+    transform: scale3d(2.15,1.85,2);
+  }
+  30%{
+    transform: scale3d(1.85,2.15,2);
+  }
+  45%{
+    transform: scale3d(2.15,1.85,2);
+  }
+  65%{
+    transform: scale3d(2.05,1.95,2);
+  }
+  100%{
+    transform: scale3d(2,2,2);
+  }
+
+
 }
 
 .lowerside {
@@ -327,6 +425,43 @@ export default {
   transition: border-color .5s;
 }
 textbox:hover{
+
+}
+.answer-alternative-size-wrapper {
+  border: solid 2px ;
+  border-color: rgba(82, 77, 77, 0.55);
+  background-color: white;
+  height: 30%;
+  width: 35%;
+  border-radius: 10px ;
+  display: grid;
+  color: #444;
+
+
+}
+#Answer-Box-symbol-prop {
+  column-width: 40px;
+  background-color: rgba(6, 236, 4, 0.73);
+  border-radius: 7px 0px 0px 7px ;
+  height: 100%;
+  width: 100%;
+}
+.Answer-Box-textarea {
+  border-left: solid 2px ;
+  border-color: rgba(82, 77, 77, 0.55);
+  column-width:auto;
+  grid-column: 2 ;
+  width: 93%;
+
+}
+
+.Answer-Box-checkbox {
+  border-left: solid 2px ;
+  border-color: rgba(82, 77, 77, 0.55);
+  column-width: 50px;
+  height: 100%;
+  grid-column: 3 ;
+
 
 }
 
@@ -420,7 +555,7 @@ textbox:hover{
   left:50% ;
   transform: translateX(-50%);
   z-index: 2;
-  
+
 }
 #selectbutton{
   height: 48px;
