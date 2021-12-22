9dbefb8398243d4c2de5f19fdfccd09a6b8e710e
Felix Ström <felix.strom.skola@gmail.com>
2021-12-22 17:58:03 +0100

    FIXAT FUCKING DUBBELKLICKEN

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -370,9 +370,17 @@ export default {
       this.currentLQ = imp
       this.showLocationQuestion()
       var coll = document.getElementsByClassName("collapsible");
-      var i;
+      //var i;
 
-      for (i = 0; i < coll.length; i++) {
+      coll[imp].classList.toggle("active");
+      var content = coll[imp].nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+
+      /*for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
           this.classList.toggle("active");
           var content = this.nextElementSibling;
@@ -382,7 +390,7 @@ export default {
             content.style.maxHeight = content.scrollHeight + "px";
           }
         });
-      }
+      }*/
     },
 
     runQuestion: function () {
