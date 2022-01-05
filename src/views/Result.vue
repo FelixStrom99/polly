70df0a817734dc1d6d3db4e28737dedd5195cd2c
AdrianLoof <adrian.loof.0474@student.uu.se>
2022-01-05 21:24:45 +0100

    hej

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -19,7 +19,7 @@
 
 
 
-
+{{question}}
 
 
 
@@ -68,6 +68,7 @@ export default {
     socket.emit('joinPoll', this.pollId)
     socket.on("dataUpdate", (update) => {
       this.createAnswerArray(update)
+      console.log("krök",update.q)
       /*  this.data = update.a;
       this.question = update.q;*/
 
@@ -100,12 +101,14 @@ export default {
   methods: {
 
     createAnswerArray: function (answerData) {
-      console.log("mja",this.question)
       for (let i = 0; i < answerData.length; i++) {
         this.question.push((answerData[i].q))
-        console.log(answerData[i].q,"hej")
-        delete answerData[i].q
-        this.followUpData.push(answerData[i])
+       let keys=Object.keys(answerData[i])
+        let answers={}
+        for (let index = 1; index < keys.length; index++) {
+          answers[keys[index]]=answerData[i][keys[index]]
+        }
+        this.followUpData.push(answers)
       }
 
     },
